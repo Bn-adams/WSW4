472077987c5fa2d21208eed4431c7003c8a972b6
Bn-adams <126073556+Bn-adams@users.noreply.github.com>
2023-11-30 15:09:40 +0000

    added

--- WSW4/Assets/Scripts/ObstacleSpawner.cs
+++ WSW4/Assets/Scripts/ObstacleSpawner.cs
@@ -13,8 +13,8 @@ public class ObstacleSpawner : MonoBehaviour, Spawner
         // Assuming the obs origin is centered, we need half its height, and half the ground's height to position it on the ground.
 
         float yPos = caller.transform.localScale.y / 2 + obstacle.transform.localScale.y / 2;
-        float xPos = UnityEngine.Random.Range(-5, 5);
-        float zPos = UnityEngine.Random.Range(-5, 5);
+        float xPos = UnityEngine.Random.Range(-5, 5) + caller.transform.localScale.x / obstacle.transform.localScale.x;
+        float zPos = UnityEngine.Random.Range(-5, 5) + caller.transform.localScale.z/ obstacle.transform.localScale.z;
 
         obstacle.transform.position = new Vector3(xPos, yPos, zPos);
         return obstacle;
